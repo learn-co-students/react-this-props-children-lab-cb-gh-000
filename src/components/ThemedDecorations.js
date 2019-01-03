c11e12d3508b26b472986cbd717f6375d7eb1dbc
Adam N <adam.nawroc@gmail.com>
2019-01-03 13:29:21 +0000

    Done.

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,22 @@
 // Code ThemedDecoration Component Here
+
+
+// Code Invitation Component Here
+
+import React from 'react';
+
+export default class ThemedDecoration extends React.Component {
+  render() {
+    const childrenWithWrapperDiv = React.Children.map(this.props.children, child => {
+      return (
+        <div className="heaven">{child}</div>
+      );
+    });
+
+    return (
+      <div>
+        {childrenWithWrapperDiv}
+      </div>
+    );
+  }
+}
