fc398835bf2d0bd860c1137d6cca4d51970d73d0
N M <niclas@mettenleiter.com>
2019-08-17 09:55:45 +0000

    Automatically backed up by Learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,12 @@
 // Code ThemedDecoration Component Here
+import React, { Component } from "react";
+
+export default class ThemedDecoration  extends  Component {
+  render () {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+}
