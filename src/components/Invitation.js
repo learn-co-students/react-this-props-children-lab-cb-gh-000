eba209c9bbd3c9901b2954b73910de9a54047f94
N M <niclas@mettenleiter.com>
2019-08-17 10:00:14 +0000

    Automatically backed up by Learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -6,7 +6,7 @@ export default class Invitation extends Component {
   render () {
     return (
       <div>
-        <h1>"'You've been invited!'"</h1>
+        <h1>You've been invited!'</h1>
         {this.props.children}
       </div>
     )
