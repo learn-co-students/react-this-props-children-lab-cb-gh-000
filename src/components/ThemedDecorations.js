2e06d0b96b7885d8f736d11a36717d1129237ba9
Recep Ekşi <portre_90@hotmail.com>
2020-01-09 21:00:14 +0000

    Done.

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,17 @@
 // Code ThemedDecoration Component Here
+import React from 'react';
+export default class ThemedDecoration extends React.Component{
+  render(){
+    const passThis=this.props.theme
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className : passThis
+      });
+    });
+    return(
+        <div>
+          {childrenWithExtraProp}
+        </div>
+    )
+  }
+}
